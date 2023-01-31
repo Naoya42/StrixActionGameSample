9f6876aa97876321e3aa26c0ac0c7ca77465d2ed
Naoya42 <61400691+Naoya42@users.noreply.github.com>
2023-01-31 12:38:01 +0900

    Initial commit

---
+++ Assets/Editor/Dirtifier.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Dirtifier : MonoBehaviour
+{
+    [MenuItem("Assets/Dirtify")]
+    public static void Dirtify() {
+        foreach (var assetPath in AssetDatabase.GetAllAssetPaths()) {
+            var asset = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+            EditorUtility.SetDirty(asset);
+        }
+
+        AssetDatabase.SaveAssets();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# StrixActionGameSample
+ 
